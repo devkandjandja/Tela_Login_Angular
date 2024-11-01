eb7bf89ed3d5df9c1d63afa7782a423327e4493b
Laurindo Firmino <Laurindo kandjandja>
2024-11-01 04:39:22 +0100

    best_pratica

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http:HttpClient) { }
+  baseURL = 'http://localhost:7288/api';
+
+  createrUser(formData:any){
+    return this.http.post(this.baseURL+'/signup', formData);
+  }
+}
+
